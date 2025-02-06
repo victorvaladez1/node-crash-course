52d472c45d7861a48623db2ef01149044ed501c8
Victor Valadez <vvaladez315@gmail.com>
2025-02-06 17:52:29 -0600

    complete tutorial 9

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,6 +10,17 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
+
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path ', req.path);
+    console.log('method', req.method);
+    next();
+});
 
 app.get('/', (req, res) => {
     // res.send('<p> home page </p>');
