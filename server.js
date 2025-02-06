4774529e44ea8fb00c4252930b899af04d3e1726
Victor Valadez <vvaladez315@gmail.com>
2025-02-05 19:03:33 -0600

    complete tutorial #4

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            return; 
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
 });
 
 server.listen(3000, 'localhost', () => {
